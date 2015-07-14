e1bf868ddafc0ef5de864aea647833384b78f326
Chris Rice <chrisrice93@gmail.com>
2015-07-14 17:28:46 -0500

    empty functions

--- file.py
+++ file.py
@@ -11,3 +11,16 @@ class File:
         my_file.close()
 
         return byte_count
+
+    def setBytes(self, bytes):
+        # Take an array of bytes and write them to the file
+
+        pass
+
+    def getBytes(self):
+        # Read the file and return an array of bytes, maybe should cache this
+
+    def appendBytes(self, bytes):
+        # Take an array of bytes and add them to the
+        # end of the file
+        pass
